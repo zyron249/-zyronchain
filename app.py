abf674ef75dcecf503222748d751aa5f4373fe41
zyron249 <ciyagerkurdi@icloud.com>
2026-06-20 04:00:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
-from flask import Flask
+from flask import Flask, request
+from zyron.blockchain import Blockchain
 
 app = Flask(__name__)
+
+chain = Blockchain()
+
 
 @app.route("/")
 def home():
     return {
         "name": "ZyronChain",
-        "status": "running"
+        "blocks": len(chain.chain),
+        "valid": chain.is_chain_valid()
     }
 
+
+@app.route("/mine/<address>")
+def mine(address):
+    chain.mine_pending_transactions(address)
+
+    return {
+        "message": "Block mined",
+        "miner": address
+    }
+
+
+@app.route("/balance/<address>")
+def balance(address):
+    return {
+        "address": address,
+        "balance": chain.get_balance(address)
+    }
+
+
+@app.route("/transaction", methods=["POST"])
+def transaction():
+    data = request.json
+
+    txid = chain.add_transaction(
+        data["sender"],
+        data["receiver"],
+        data["amount"]
+    )
+
+    return {
+        "txid": txid
+    }
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
