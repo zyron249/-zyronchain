2b1822bc02f71e19cab7e484af64878f784591ab
zyron249 <ciyagerkurdi@icloud.com>
2026-06-20 03:53:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Welcome to ZyronChain")
+from zyron.blockchain import Blockchain
+
+chain = Blockchain()
+
+print("Creating Block 1...")
+chain.add_block([
+    "Alice -> Bob : 10 ZYN"
+])
+
+print("Creating Block 2...")
+chain.add_block([
+    "Bob -> Charlie : 5 ZYN"
+])
+
+print("\nBlockchain Status")
+print("-----------------")
+print("Total Blocks:", len(chain.chain))
+print("Chain Valid:", chain.is_chain_valid())
+
+for block in chain.chain:
+    print("\nBlock:", block.index)
+    print("Hash:", block.hash)
+    print("Previous:", block.previous_hash)
+    print("Transactions:", block.transactions)
