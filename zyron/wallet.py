73a807cdcdf20fbac397a6fd9ad9ece9aad9b381
zyron249 <ciyagerkurdi@icloud.com>
2026-06-20 05:13:46 -0400

    Update wallet.py

--- zyron/wallet.py
+++ zyron/wallet.py
@@ -1,7 +1,27 @@
 import hashlib
-import time
+import ecdsa
 
 
-def create_wallet_address(name):
-    data = f"{name}{time.time()}"
-    return "ZYN" + hashlib.sha256(data.encode()).hexdigest()[:32]
+class Wallet:
+    def __init__(self):
+        self.private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+        self.public_key = self.private_key.get_verifying_key()
+        self.address = self.create_address()
+
+    def create_address(self):
+        public_key_bytes = self.public_key.to_string()
+        sha256_hash = hashlib.sha256(public_key_bytes).hexdigest()
+        return "ZYN" + sha256_hash[:40]
+
+    def get_private_key(self):
+        return self.private_key.to_string().hex()
+
+    def get_public_key(self):
+        return self.public_key.to_string().hex()
+
+    def to_dict(self):
+        return {
+            "address": self.address,
+            "private_key": self.get_private_key(),
+            "public_key": self.get_public_key()
+        }
