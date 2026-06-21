b3ff85054dc396315580b891cb3d27ee1d0ac41d
zyron249 <ciyagerkurdi@icloud.com>
2026-06-21 19:50:13 -0400

    Update blockchain.py

--- zyron/blockchain.py
+++ zyron/blockchain.py
@@ -6,20 +6,14 @@ from zyron.storage import BlockchainStorage
 class Blockchain:
     def __init__(self):
         self.storage = BlockchainStorage()
-
         self.chain = [self.create_genesis_block()]
         self.difficulty = 4
         self.pending_transactions = []
         self.mining_reward = 50
-
         self.load_chain()
 
     def create_genesis_block(self):
-        return Block(
-            0,
-            ["Genesis Block"],
-            "0"
-        )
+        return Block(0, ["Genesis Block"], "0")
 
     def get_latest_block(self):
         return self.chain[-1]
@@ -42,38 +36,19 @@ class Blockchain:
             block_data["previous_hash"],
             block_data.get("difficulty", 4)
         )
-
         block.timestamp = block_data["timestamp"]
         block.nonce = block_data["nonce"]
         block.hash = block_data["hash"]
-
         return block
 
     def save_chain(self):
-        chain_data = []
-
-        for block in self.chain:
-            chain_data.append(
-                self.block_to_dict(block)
-            )
-
-        self.storage.save_chain(chain_data)
+        self.storage.save_chain([self.block_to_dict(block) for block in self.chain])
 
     def load_chain(self):
         data = self.storage.load_chain()
-
         if not data:
             return
-
-        loaded_chain = []
-
-        for block_data in data:
-            loaded_chain.append(
-                self.dict_to_block(block_data)
-            )
-
-        if loaded_chain:
-            self.chain = loaded_chain
+        self.chain = [self.dict_to_block(block_data) for block_data in data]
 
     def is_valid_chain(self, chain_to_validate):
         target = "0" * self.difficulty
@@ -94,7 +69,6 @@ class Blockchain:
             for tx_data in current.transactions:
                 if isinstance(tx_data, dict):
                     tx = Transaction.from_dict(tx_data)
-
                     if not tx.is_valid():
                         return False
 
@@ -102,88 +76,52 @@ class Blockchain:
 
     def replace_chain(self, new_chain_data):
         if not new_chain_data:
-            return {
-                "replaced": False,
-                "reason": "No chain data provided"
-            }
+            return {"replaced": False, "reason": "No chain data provided"}
 
-        new_chain = []
-
-        for block_data in new_chain_data:
-            new_chain.append(
-                self.dict_to_block(block_data)
-            )
+        new_chain = [self.dict_to_block(block_data) for block_data in new_chain_data]
 
         if len(new_chain) <= len(self.chain):
-            return {
-                "replaced": False,
-                "reason": "Incoming chain is not longer"
-            }
+            return {"replaced": False, "reason": "Incoming chain is not longer"}
 
         if not self.is_valid_chain(new_chain):
-            return {
-                "replaced": False,
-                "reason": "Incoming chain is invalid"
-            }
+            return {"replaced": False, "reason": "Incoming chain is invalid"}
 
         self.chain = new_chain
         self.pending_transactions = []
         self.save_chain()
 
-        return {
-            "replaced": True,
-            "new_length": len(self.chain)
-        }
+        return {"replaced": True, "new_length": len(self.chain)}
 
     def get_pending_spent_amount(self, address):
         total = 0
-
         for tx in self.pending_transactions:
             if isinstance(tx, dict) and tx.get("sender") == address:
                 total += tx["amount"]
-
         return total
 
     def get_available_balance(self, address):
-        confirmed_balance = self.get_balance(address)
-        pending_spent = self.get_pending_spent_amount(address)
-
-        return confirmed_balance - pending_spent
+        return self.get_balance(address) - self.get_pending_spent_amount(address)
 
     def add_transaction(self, transaction):
         if not transaction.is_valid():
             raise Exception("Invalid transaction signature")
 
         if transaction.sender == "SYSTEM":
-            self.pending_transactions.append(
-                transaction.to_dict()
-            )
+            self.pending_transactions.append(transaction.to_dict())
             return transaction.txid
 
         if transaction.amount <= 0:
             raise Exception("Transaction amount must be greater than zero")
 
-        available_balance = self.get_available_balance(transaction.sender)
-
-        if available_balance < transaction.amount:
+        if self.get_available_balance(transaction.sender) < transaction.amount:
             raise Exception("Insufficient balance")
 
-        self.pending_transactions.append(
-            transaction.to_dict()
-        )
-
+        self.pending_transactions.append(transaction.to_dict())
         return transaction.txid
 
     def mine_pending_transactions(self, miner_address):
-        reward_tx = Transaction(
-            "SYSTEM",
-            miner_address,
-            self.mining_reward
-        )
-
-        self.pending_transactions.append(
-            reward_tx.to_dict()
-        )
+        reward_tx = Transaction("SYSTEM", miner_address, self.mining_reward)
+        self.pending_transactions.append(reward_tx.to_dict())
 
         block = Block(
             len(self.chain),
@@ -195,7 +133,6 @@ class Blockchain:
         block.mine()
         self.chain.append(block)
         self.save_chain()
-
         self.pending_transactions = []
 
     def get_balance(self, address):
@@ -206,7 +143,6 @@ class Blockchain:
                 if isinstance(tx, dict):
                     if tx["sender"] == address:
                         balance -= tx["amount"]
-
                     if tx["receiver"] == address:
                         balance += tx["amount"]
 
@@ -231,10 +167,28 @@ class Blockchain:
                     "transaction": tx
                 }
 
-        return {
-            "found": False,
-            "txid": txid
-        }
+        return {"found": False, "txid": txid}
+
+    def get_address_transactions(self, address):
+        transactions = []
+
+        for block in self.chain:
+            for tx in block.transactions:
+                if not isinstance(tx, dict):
+                    continue
+
+                if tx.get("sender") == address or tx.get("receiver") == address:
+                    transactions.append({
+                        "block_index": block.index,
+                        "txid": tx.get("txid"),
+                        "sender": tx.get("sender"),
+                        "receiver": tx.get("receiver"),
+                        "amount": tx.get("amount"),
+                        "timestamp": tx.get("timestamp")
+                    })
+
+        transactions.sort(key=lambda x: x.get("timestamp", 0), reverse=True)
+        return transactions
 
     def is_chain_valid(self):
         return self.is_valid_chain(self.chain)
