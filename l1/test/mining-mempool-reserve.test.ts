431ea62551eaa59b668f1a08b191799d0deb8c9d
Zyron Chain <ciyagerkurdi@icloud.com>
2026-08-11 10:05:33 -0400

    Test reserved mining mempool capacity

---
+++ l1/test/mining-mempool-reserve.test.ts
@@ -0,0 +1,76 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+import { Mempool } from "../src/mempool.js";
+import type { MiningClaimTx, TransferTx } from "../src/types.js";
+
+const chainId = "zyron-mining-reserve-test";
+
+function transfer(index: number): TransferTx {
+  return {
+    kind: "transfer",
+    version: 1,
+    chainId,
+    nonce: 1,
+    sender: `ZYN${index.toString(16).padStart(40, "0")}` as TransferTx["sender"],
+    receiver: `ZYN${(index + 100).toString(16).padStart(40, "0")}` as TransferTx["receiver"],
+    amountAtoms: 1,
+    feeAtoms: 1,
+    timestampMs: 1_700_000_000_000 + index,
+    publicKey: "11".repeat(64),
+    signature: "22".repeat(64),
+    txid: index.toString(16).padStart(64, "0")
+  };
+}
+
+function miningClaim(index: number): MiningClaimTx {
+  return {
+    kind: "mining_claim",
+    version: 2,
+    chainId,
+    nonce: 1,
+    sender: `ZYN${(index + 500).toString(16).padStart(40, "0")}` as MiningClaimTx["sender"],
+    height: 1,
+    previousHash: "ab".repeat(32),
+    rewardAtoms: 625_000_000,
+    workNonce: index.toString(16).padStart(16, "0"),
+    feeAtoms: 0,
+    timestampMs: 1_700_000_001_000 + index,
+    publicKey: "33".repeat(64),
+    signature: "44".repeat(64),
+    txid: (index + 10_000).toString(16).padStart(64, "0")
+  };
+}
+
+test("reserved mining capacity remains available when non-mining capacity is saturated", () => {
+  const mempool = new Mempool(2, 1);
+  const firstTransfer = transfer(1);
+  const secondTransfer = transfer(2);
+  const claim = miningClaim(1);
+
+  mempool.add(firstTransfer);
+  mempool.add(secondTransfer);
+  assert.equal(mempool.size, 2);
+
+  mempool.add(claim);
+  assert.equal(mempool.size, 3);
+  assert.ok(mempool.values().some((tx) => tx.txid === claim.txid));
+  assert.ok(mempool.values().some((tx) => tx.txid === firstTransfer.txid));
+  assert.ok(mempool.values().some((tx) => tx.txid === secondTransfer.txid));
+});
+
+test("mining reserve is bounded and cannot consume extra non-mining capacity", () => {
+  const mempool = new Mempool(1, 1);
+  mempool.add(transfer(1));
+  mempool.add(miningClaim(1));
+
+  assert.throws(() => mempool.add(miningClaim(2)), /Mining mempool full/);
+  assert.equal(mempool.size, 2);
+});
+
+test("custom mempool capacity remains a hard total cap unless a mining reserve is explicitly configured", () => {
+  const mempool = new Mempool(1);
+  mempool.add(transfer(1));
+  assert.throws(() => mempool.add(miningClaim(1)), /Mining mempool full/);
+  assert.equal(mempool.size, 1);
+});
