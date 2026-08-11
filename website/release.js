e80be48859b65b13bb8a96d3d4cb2c889420bb39
Zyron Chain <ciyagerkurdi@icloud.com>
2026-08-11 05:06:48 -0400

    Pin website launchers to protocol-v5 main

--- website/release.js
+++ website/release.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const RELEASE_REF = '1029648968e9f0a9825ad3227537efd18dd1d28f';
+  const RELEASE_REF = '4b240627a76afddc2ffbed893dbcd6aeca3c9d5c';
   if (!/^[0-9a-f]{40}$/.test(RELEASE_REF)) {
     throw new Error('Invalid ZyronChain canonical release reference');
   }
