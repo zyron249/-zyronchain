73f7eb6ed4446fec8f975eb02ad7887897f199b1
Zyron Chain <ciyagerkurdi@icloud.com>
2026-08-12 02:18:07 -0400

    Extend IPv6 prefix canonicalization coverage

--- l1/test/ipv6-prefix64.test.ts
+++ l1/test/ipv6-prefix64.test.ts
@@ -6,6 +6,7 @@ import { ipv6Prefix64 } from "../src/p2p-address.js";
 test("ipv6Prefix64 canonicalizes compressed and expanded addresses in one /64", () => {
   assert.equal(ipv6Prefix64("2001:db8:1234:5678::1"), "2001:0db8:1234:5678");
   assert.equal(ipv6Prefix64("2001:0db8:1234:5678:abcd:ef01:2345:6789"), "2001:0db8:1234:5678");
+  assert.equal(ipv6Prefix64("2001:DB8:1234:5678::1"), "2001:0db8:1234:5678");
 });
 
 test("ipv6Prefix64 keeps distinct /64 prefixes distinct", () => {
