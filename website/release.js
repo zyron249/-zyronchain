1e9aee00e40a6a417006900ae0c65c0ae74309ef
Zyron Chain <ciyagerkurdi@icloud.com>
2026-08-17 13:02:20 -0400

    Refresh website canonical release pin for miner alternatives

--- website/release.js
+++ website/release.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const RELEASE_REF = '32f7fd8196d86f5d8193e9a16cc810339e648e12';
+  const RELEASE_REF = '133f0053b2a090cbe2600c39ed745b5d45446b4f';
   if (!/^[0-9a-f]{40}$/.test(RELEASE_REF)) {
     throw new Error('Invalid ZyronChain canonical release reference');
   }
