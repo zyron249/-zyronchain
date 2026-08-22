f4543006a565d56b086157f87d870fc0824f0456
Zyron Chain <ciyagerkurdi@icloud.com>
2026-08-22 08:59:07 -0400

    website: refresh canonical release pin for miner digest policy

--- website/release.js
+++ website/release.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const RELEASE_REF = '561cb4c88f4757234a80ecb1df3f930283a99f30';
+  const RELEASE_REF = '33ffc1eb70ea618e387ef68358dbfeca9ca05c06';
   if (!/^[0-9a-f]{40}$/.test(RELEASE_REF)) {
     throw new Error('Invalid ZyronChain canonical release reference');
   }
